20d6d430d6b734f21e04e0c78809bd9c27a4986d
Andrew <contact@andrewthong.com>
2021-01-18 00:58:51 -0800

    time format

--- pages/processing.vue
+++ pages/processing.vue
@@ -28,8 +28,8 @@
             <b-td>{{ item.id }}</b-td>
             <b-td>{{ item.province }}</b-td>
             <b-td>{{ item.date }}</b-td>
-            <b-td>{{ item.created_at }}</b-td>
-            <b-td>{{ item.updated_at }}</b-td>
+            <b-td>{{ item.created_at | at }}</b-td>
+            <b-td>{{ item.updated_at | at }}</b-td>
           </b-tr>
         </b-tbody>
       </b-table-simple>
@@ -46,6 +46,7 @@
       return {
         loading: true,
         lastUpdated: null,
+        tzOffset: null,
         status: {
           waiting: {
             title: 'Waiting for Next Run',
@@ -60,6 +61,7 @@
     },
     created: function() {
       this.getStatus();
+      this.tzOffset = (new Date()).getTimezoneOffset() * 60000;
     },
     methods: {
       getStatus() {
@@ -71,9 +73,16 @@
             this.status.waiting.data = response.waiting;
             this.status.processed.data = response.processed;
             this.loading = false;
-            this.lastUpdated = new Date().toLocaleString();
+            this.lastUpdated = (new Date(Date.now() - this.tzOffset)).toISOString().slice(0, -5).replace('T', ' ')
           });
       },
+    },
+    filters: {
+      at: function(value) {
+        if (!value) return '';
+        value = value.toString();
+        return value.slice(0, -8).replace('T', ' ');
+      }
     },
   }
 </script>
