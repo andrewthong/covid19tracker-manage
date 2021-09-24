6b284b2ef91b1cb43affb16db73a8ac4293c30a7
Andrew <contact@andrewthong.com>
2021-09-24 08:43:29 -0700

    revoke key in code...

--- plugins/amplitude.js
+++ plugins/amplitude.js
@@ -2,6 +2,6 @@ import amplitude from 'amplitude-js';
 
 export default (context, inject) =>
 {
-  const ampl = amplitude.getInstance().init("702f5825319376b8cfd21a9e032eb6bf");
+  const ampl = amplitude.getInstance().init(process.env.AMPLITUDE_API_KEY);
   inject('amplitude', amplitude)
 }
